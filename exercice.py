736bc5f135fc0f1badb53ad21d57f56db0559d4b
hishamboulifa <hish011b@gmail.com>
2020-09-28 17:33:23 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -6,7 +6,7 @@ def order(values: list = None) -> bool:
     if values is None:
         # TODO: Demander les valeurs ici
         pass
-        values = [input('Insérer 10 nombres: ') for _ in 
+        values = [input('Insérer 10 nombres: ') for _ in range(10)
         sort(valeurs)
         
     return False
